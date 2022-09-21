ceb1a4f26f5f369bb9fb7ba5192440094c0d902c
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-09-21 20:00:53 -0300

    Criando arquivo de exercicios de fixação de JS

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicioTiposPrimitivos.js
@@ -0,0 +1,11 @@
+// Declarando variaveis
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = area + base + height;
+
+// Imprimindo resultados no console
+console.log(base);
+console.log(height);
+console.log(area);
+console.log(perimeter);
