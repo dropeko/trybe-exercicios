5641d0bafe0700af5494a04c6de01c230b20d134
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-09-21 19:35:33 -0300

    Criando arquivo de exercicios de fixação de JS

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/execercicioFixacao.js
@@ -0,0 +1,18 @@
+// Criando as variaveis
+const myName = 'Pedro';
+const birthCity = 'Belo Horizonte';
+let birthYear = 1994;
+
+// Imprimindo constantes e variavel criadas
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+// Alternado valor atribuido a variavel birthYear
+birthYear = 2030;
+console.log(birthYear);
+
+// Alterando valor atribuido a variavel birthCity
+// Irá retornar erro!
+birthCity = 'Rio de Janeiro';
+console.log(birthCity);
