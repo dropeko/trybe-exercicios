b2a00fdd6a05ce0e7942e0462ae58112c1ef9e1b
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-09-21 20:21:08 -0300

    Criando arquivo de exercicios de fixação de JS

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exerciciosCondicoesIfElse.js
@@ -0,0 +1,13 @@
+// Declarando variavel constante
+const nota = 7;
+
+// Construindo estrutura condicional 
+if (nota >= 80){
+    console.log('Parabéns!! Você foi aprovado!');
+}
+else if (nota < 80 && nota >= 60){
+    console.log('Você está na lista de espera.')
+}
+else if (nota <60){
+    console.log('Você foi reprovado!')
+}
