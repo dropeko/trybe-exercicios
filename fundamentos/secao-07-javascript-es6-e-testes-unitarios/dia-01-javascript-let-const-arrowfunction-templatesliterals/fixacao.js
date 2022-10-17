0ff36f09f2ed683f1099f7d17350fa8c4f2f8bff
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-10-17 18:31:16 -0300

    Adicionando arquivo exercicios de fixação seção 07

---
+++ fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-01-javascript-let-const-arrowfunction-templatesliterals/fixacao.js
@@ -0,0 +1,15 @@
+function hello() {
+  let s = ["Hello World!", "Olá Mundo!"];
+  console.log(s[0]);
+}
+
+const nome = "Gus";
+const modulos = [1];
+const empresa = {
+  nome: "Trybe",
+  nota: 8001,
+};
+
+modulos.push(3);
+nome = "Batman";
+empresa.nota = 8002;
