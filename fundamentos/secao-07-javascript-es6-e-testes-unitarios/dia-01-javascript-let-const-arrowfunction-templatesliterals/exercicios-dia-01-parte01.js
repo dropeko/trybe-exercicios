420bf6b23af4b2714fc8b27dd7bf5c340f476852
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-10-17 18:52:05 -0300

    Modifique as concatenações para template literals

--- fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-01-javascript-let-const-arrowfunction-templatesliterals/exercicios-dia-01-parte01.js
+++ fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-01-javascript-let-const-arrowfunction-templatesliterals/exercicios-dia-01-parte01.js
@@ -7,11 +7,10 @@
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
+    console.log(`${ifScope}. Ótimo, fui utilizada no escopo !`);
   } else {
     let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-    console.log(elseScope);
+    console.log(`${elseScope}`);
   }
 }
 
