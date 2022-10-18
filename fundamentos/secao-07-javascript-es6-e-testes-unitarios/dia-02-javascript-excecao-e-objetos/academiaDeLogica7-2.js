4f76a8faefc8e7bcf2c1b6f1df0469d6c6f434b1
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-10-18 13:21:38 -0300

    Escreva uma função que recebe por parâmetro um array de números e retorna a soma dos números pares.

--- fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-javascript-excecao-e-objetos/academiaDeLogica7-2.js
+++ fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-javascript-excecao-e-objetos/academiaDeLogica7-2.js
@@ -1,3 +1,19 @@
 const verificaPar = (num) => num % 2 === 0 && console.log(`o valor ${num} é par!`);
 
 verificaPar(5);
+
+// ---------------------------------------------------------------------------------------
+
+const array = [7, 8, 6, 4, 3, 2, 1, 77, 88]
+
+const somaPar = (array) => {
+  let somador = 0;
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] % 2 === 0 ){
+      somador += array[index];
+    }    
+  }
+  return somador
+}
+
+console.log(somaPar(array));
