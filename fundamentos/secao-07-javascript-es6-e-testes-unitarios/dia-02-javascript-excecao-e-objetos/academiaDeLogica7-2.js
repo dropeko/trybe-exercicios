438761561c23e41be068d374f4e61f008e8cffb6
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-10-18 13:40:19 -0300

    Exercicios 3 e 4

--- fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-javascript-excecao-e-objetos/academiaDeLogica7-2.js
+++ fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-02-javascript-excecao-e-objetos/academiaDeLogica7-2.js
@@ -4,6 +4,7 @@ verificaPar(5);
 
 // ---------------------------------------------------------------------------------------
 
+// Escreva uma função que dado um parâmetro retorna se o tipo de dado é Number.
 const array = [7, 8, 6, 4, 3, 2, 1, 77, 88]
 
 const somaPar = (array) => {
@@ -17,3 +18,28 @@ const somaPar = (array) => {
 }
 
 console.log(somaPar(array));
+
+// ---------------------------------------------------------------------------------------
+
+// Escreva uma função que receba um nome de usuário e retorne a seguinte mensagem: "Boas vindas, USERNAME!". Valide que o tipo de dado seja string e o texto tenha pelo menos 1 caracter, e caso o parâmetro não atenda alguma das condições o retorno deve ser a string "Parâmetro inválido.".
+
+const type = (param) => {
+  if (typeof param === "number") {
+    return `O paramêntro ${param} é do tipo Number!`
+  }
+  return 'bugou'
+}
+
+console.log(type('5'));
+
+// ---------------------------------------------------------------------------------------
+// Escreva uma função que receba um nome de usuário e retorne a seguinte mensagem: "Boas vindas, USERNAME!". Valide que o tipo de dado seja string e o texto tenha pelo menos caracter, e caso o parâmetro não atenda alguma das condições o retorno deve ser a string "Parâmetro inválido.".
+
+const verificaNome = (name) => {
+  if (typeof name === "string" && name.length > 0) {
+    return `Boas vindas, ${name}!`
+  }
+  return 'Parametro inválido'
+}
+
+console.log(verificaNome('Gus'));
