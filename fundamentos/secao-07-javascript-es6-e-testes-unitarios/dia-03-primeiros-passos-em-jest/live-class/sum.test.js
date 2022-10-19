d618d7779f3045eecf5ab440c0270af7dffe919f
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-10-19 20:49:07 -0300

    Teste se o retorno de sum(0, 0) é 0

--- fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/live-class/sum.test.js
+++ fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/live-class/sum.test.js
@@ -3,5 +3,10 @@ const sum = require('./sum');
 describe('Testes realizados com a função sum ->', () => {
   test('testa se 4 + 5 é igual a: 9', () => {
     expect(sum(4, 5)).toBe(9);
-  })
-})
+  });
+  test('testa se 0 + 0 é igual a: 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+
+});
