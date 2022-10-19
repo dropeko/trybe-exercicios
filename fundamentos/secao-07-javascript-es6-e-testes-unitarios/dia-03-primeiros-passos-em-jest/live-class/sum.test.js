a74278807dcd67e380e40da3baa9a9b9044b1c59
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-10-19 20:54:22 -0300

    Teste se a mensagem de erro é “parameters must be numbers” quando realizar a chamada sum(4, "5")

--- fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/live-class/sum.test.js
+++ fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/live-class/sum.test.js
@@ -7,6 +7,8 @@ describe('Testes realizados com a função sum ->', () => {
   test('testa se 0 + 0 é igual a: 0', () => {
     expect(sum(0, 0)).toBe(0);
   });
-
+  test('testa se a função lança um erro.', () => {
+    expect(() => {sum(4, '5')}).toThrow('parameters must be numbers')
+  })
 
 });
