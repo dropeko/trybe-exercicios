99d4be95dddb8eeb0af218c5fe9a360a96b94aa5
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-10-19 21:50:34 -0300

    Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado

--- fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/live-class/myFizzBuzz.test.js
+++ fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/live-class/myFizzBuzz.test.js
@@ -5,4 +5,13 @@ describe('Testes com a função FizzBuzz', () => {
   test('Verifica se é divisivel por 3 e 5', () => {
     expect(myFizzBuzz(15)).toBe('fizzbuzz')
   });
+
+  test('Verifica se é divisivel por 3', () => {
+    expect(myFizzBuzz(9)).toBe('fizz')
+  });
+
+  test('Verifica se é divisivel por 5', () => {
+    expect(myFizzBuzz(10)).toBe('buzz')
+  });
+  
 });
