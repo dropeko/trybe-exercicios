479b006a63ada214f86cec2514b0a02d62c4b89d
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2022-10-19 21:37:51 -0300

    Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]

--- fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/live-class/myRemove.test.js
+++ fundamentos/secao-07-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/live-class/myRemove.test.js
@@ -1,7 +1,13 @@
 const myRemove = require('./myRemove');
 
 describe('Testes com a função myRemove', () => {
+
   test('Verifica se retorna o array esperado', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
+
+  test('Verificar se não retorna o array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
 });
