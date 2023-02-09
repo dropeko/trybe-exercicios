fcabdc04dd579485345f2781390f84135a0bd687
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-02-09 16:25:18 -0300

    Finalizando exercicio da aula 01 seção 01 front end

--- front-end/secao-01-introducao-ao-frontEnd-e-JsAssincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ front-end/secao-01-introducao-ao-frontEnd-e-JsAssincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,5 +1,7 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import './styles.css';
+
 
 const generatePassword = document.getElementById('new-password-button');
 const newPassword = document.getElementById('new-password-generated');
@@ -7,4 +9,9 @@ const newPassword = document.getElementById('new-password-generated');
 generatePassword.addEventListener('click', () => {
   const randomPassword = nanoid();
   newPassword.innerHTML = randomPassword;
+});
+
+newPassword.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada com sucesso!');
 });
