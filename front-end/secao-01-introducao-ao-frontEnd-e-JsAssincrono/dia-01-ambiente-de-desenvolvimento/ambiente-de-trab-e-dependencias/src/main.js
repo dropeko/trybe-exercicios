ac476f4a6eea8761a206cc7937bc8007f26bd0ca
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-02-09 12:50:35 -0300

    Adicionando arquivos da aula ao vivo

---
+++ front-end/secao-01-introducao-ao-frontEnd-e-JsAssincrono/dia-01-ambiente-de-desenvolvimento/ambiente-de-trab-e-dependencias/src/main.js
@@ -0,0 +1,13 @@
+// // Common JS
+// const meuArquivo = require("caminho do arquivo");
+
+// module.exports = { };
+
+// // ESModules
+// import meuArquivo from 'caminho do arquivo';
+
+// export default minhaVariavel;
+
+import './style.css';
+
+console.log('Hello, World!!');
