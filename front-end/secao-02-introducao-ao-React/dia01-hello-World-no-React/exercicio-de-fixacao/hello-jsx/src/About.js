8d3ad6cb117c075a2ebdec510eb8be298b107d3b
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-02-28 14:59:51 -0300

    Exercicio de fixação React dia 01

---
+++ front-end/secao-02-introducao-ao-React/dia01-hello-World-no-React/exercicio-de-fixacao/hello-jsx/src/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class AboutMe extends React.Component {
+  render(){
+    return (
+      <section>
+        <h1>Pedro Henrique Coli Andrade</h1>
+        <p>Comunicativo e focado em evoluir profissionalmente
+          Estou cursando a Trybe para expandir meus horizontes e 
+          oportunidades profissionais no mercado de Desenvolvimento Web.
+          1% todo dia em 100 dias é 100%
+          Você pode encontrar as coisas que perdeu, mas nunca as que abandonou </p>
+        <h2>Minhas Habilidades:</h2>
+        <ul>
+          <li>JavaScript</li>
+          <li>HTML5</li>
+          <li>CSS</li>
+          <li>React</li>
+          <li>Jest</li>
+          <li>BootStrap</li>
+          <li>Comunicação</li>
+          <li>Liderança</li>
+          <li>Proatividade</li>
+          <li>Consistente</li>
+        </ul>
+      </section>
+    )
+  }
+}
+
+export default AboutMe;
---
+++ front-end/secao-02-introducao-ao-React/dia01-hello-World-no-React/exercicio-de-fixacao/hello-jsx/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import AboutMe from "./About";
+
+class App extends React.Component {
+  render() {
+    return <AboutMe />
+  }
+}
+export default App;
