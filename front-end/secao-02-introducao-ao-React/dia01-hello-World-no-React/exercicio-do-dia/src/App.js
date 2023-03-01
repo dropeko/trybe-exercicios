8bcfdcf668d77d1d775b43577b6e9a6124a46826
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-02-28 23:37:51 -0300

    Pequena alteração

---
+++ front-end/secao-02-introducao-ao-React/dia01-hello-World-no-React/exercicio-do-dia/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderRender from './Header';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <HeaderRender />
+    );
+  }
+}
+
+export default App;
---
+++ front-end/secao-02-introducao-ao-React/dia01-hello-World-no-React/exercicio-do-dia/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HeaderRender extends React.Component {
+  render() {
+    return (
+      <h1>Conteúdos de Frontend</h1>
+    );
+  }
+}
+// Exportando
+export default HeaderRender;
