92844e68e6eb58adc6f9a66fbc9041f608ba8cba
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-03-02 16:09:30 -0300

    Exercicio de fixação 3 - dia 01 seção 03

--- front-end/secao-03-componentes-com-estado-e-forms-com-React/dia01-componentes-com-estado-e-eventos/exercicio-fixacao-01/fancy-buttons/src/App.js
+++ front-end/secao-03-componentes-com-estado-e-forms-com-React/dia01-componentes-com-estado-e-eventos/exercicio-fixacao-01/fancy-buttons/src/App.js
@@ -6,31 +6,43 @@ class App extends React.Component {
 
   constructor() {
     super()
+    this.state = {
+      clickNums: 0,
+      clickNums2: 0,
+      clickNums3: 0,
+    }
     this.handleClickOne = this.handleClickOne.bind(this)
     this.handleClickTwo = this.handleClickTwo.bind(this)
     this.handleClickThree = this.handleClickThree.bind(this)
+
   }
 
   handleClickOne () {
-    console.log(this);
     console.log('A vida é uma maravilha!!');
+    this.setState((lastState, _props) => ({
+      clickNums: lastState.clickNums + 1,
+    }))
   }
   
   handleClickTwo () {
-    console.log(this);
     console.log('Work Hard Play Hard!!');
+    this.setState((lastState2, _props) => ({
+      clickNums2: lastState2.clickNums2 + 1,
+    }))
   }
   
   handleClickThree () {
-    console.log(this);
     console.log('Você pode encontrar as coisas que perdeu, mas nunca as que abandonou!');
+    this.setState((lastState3, _props) => ({
+      clickNums3: lastState3.clickNums3 + 1,
+    }))
   }
   
   render() {
     return <div className="App">
-      <button onClick={ this.handleClickOne }>Surprise Button 01</button>
-      <button onClick={ this.handleClickTwo }>Surprise Button 02</button>
-      <button onClick={ this.handleClickThree }>Surprise Button 03</button>
+      <button onClick={ this.handleClickOne }>{ this.state.clickNums }</button>
+      <button onClick={ this.handleClickTwo }>{ this.state.clickNums2 }</button>
+      <button onClick={ this.handleClickThree }>{ this.state.clickNums3 }</button>
     </div>
     };
 };
