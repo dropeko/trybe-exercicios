426c0c2012174f6a61bb0b064368159ce2d1ca16
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-03-06 20:26:45 -0300

    Mais exercicios de fixação

---
+++ front-end/secao-02-introducao-ao-React/dia02-introducao-ao-react/exercicios-fixacao/exercise-listing-components/src/UserProfile.js
@@ -0,0 +1,19 @@
+// arquivo UserProfile.js
+import React from 'react';
+import Image from './Image';
+
+class UserProfile extends React.Component {
+  render() {
+    console.log(this.props);
+    const { user } = this.props;
+    return (
+      <div>
+        <p> { user.name } </p>
+        <p> { user.email } </p>
+        <Image source={ user.avatar } alternativeText="User avatar" />
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
