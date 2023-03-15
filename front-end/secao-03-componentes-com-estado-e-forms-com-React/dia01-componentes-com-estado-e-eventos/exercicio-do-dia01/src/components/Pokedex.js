40e52ff36478b7c70ec94ec3b6fcfe5717e31099
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-03-15 15:42:57 -0300

    exercicio dia 01 - requisito 03 e 04

--- front-end/secao-03-componentes-com-estado-e-forms-com-React/dia01-componentes-com-estado-e-eventos/exercicio-do-dia01/src/components/Pokedex.js
+++ front-end/secao-03-componentes-com-estado-e-forms-com-React/dia01-componentes-com-estado-e-eventos/exercicio-do-dia01/src/components/Pokedex.js
@@ -9,30 +9,62 @@ class Pokedex extends React.Component {
     super()
 
     this.nextPokemonOnList = this.nextPokemonOnList.bind(this)
+    this.filterPokemon = this.filterPokemon.bind(this)
+    this.fetchFilteredPokemon = this.fetchFilteredPokemon.bind(this)
 
     this.state = {
       pokedexIndex: 0,
+      filteredType: '',
     }
   }
 
-  nextPokemonOnList() {
+  nextPokemonOnList(numbsOfPokemons) {
     this.setState((lastState, _props) => ({
-      pokedexIndex: lastState.pokedexIndex + 1
+      pokedexIndex: (lastState.pokedexIndex + 1) % numbsOfPokemons,
     }))
+  }
+
+  filterPokemon(filteredType) {
+    this.setState({ filteredType, pokemonIndex: 0 });
+  }
+
+  fetchFilteredPokemon() {
+    const { pokemonList } = this.props;
+    const { filteredType } = this.state;
+  
+    return pokemonList.filter((pokemon) => {
+      if (filteredType === '') return true;
+      return pokemon.type === filteredType;
+    });
   }
 
   render() {
     const { pokedexIndex } = this.state;
-    const { pokemonList } = this.props;
+    const filteredPokemon = this.fetchFilteredPokemon();
+    const pokemon = filteredPokemon[pokedexIndex];
     return (
       <>
         <h1> Pokédex </h1>
         <div className="pokedex">
-          <Pokemon pokemon={ pokemonList[pokedexIndex] } />
+          <Pokemon pokemon={ pokemon } />
         </div>
-        <button onClick={ this.nextPokemonOnList.length }>
+        <button onClick={ () => this.nextPokemonOnList(filteredPokemon.length) }>
           Próximo Pokémon
         </button>
+        <div>
+          <button
+            type="button"
+            onClick={ () => this.filterPokemon('Fire') }
+          >
+            Fire
+          </button>
+          <button
+            type="button"
+            onClick={ () => this.filterPokemon('Psychic') }
+          >
+            Psychic
+          </button>
+        </div>
       </>
     );
   }
