f59eeb66cc674cd20592dd60dbace1a741f8fdd3
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-03-15 14:26:53 -0300

    exercicio de fixação state vs props

--- front-end/secao-03-componentes-com-estado-e-forms-com-React/dia01-componentes-com-estado-e-eventos/exercicio-fixacao-01/state-components/src/ButtonComponent.js
+++ front-end/secao-03-componentes-com-estado-e-forms-com-React/dia01-componentes-com-estado-e-eventos/exercicio-fixacao-01/state-components/src/ButtonComponent.js
@@ -30,12 +30,34 @@ class ButtonComponent extends React.Component {
       numClick03: lastState.numClick03 + 1
     }))
   }
+
+  changeColorButton(num) {
+    return num % 2 === 0 ? 'green' : 'red';
+  }
+
   render() {
     return (
       <>
-        <button onClick={ this.click }>{ this.state.numClick01 }</button>
-        <button onClick={ this.click2 }>{ this.state.numClick02 }</button>
-        <button onClick={ this.click3 }>{ this.state.numClick03 }</button>
+        <button 
+          onClick={ this.click } 
+          style={ { backgroundColor: this.changeColorButton(this.state.numClick01) } }
+        >
+          { this.state.numClick01 }
+        </button>
+
+        <button 
+        onClick={ this.click2 }
+        style={ { backgroundColor: this.changeColorButton(this.state.numClick02) } }
+        >
+          { this.state.numClick02 }
+        </button>
+        
+        <button 
+        onClick={ this.click3 }
+        style={ { backgroundColor: this.changeColorButton(this.state.numClick03) } }
+        >
+          { this.state.numClick03 }
+        </button>
       </>
     )
   }
