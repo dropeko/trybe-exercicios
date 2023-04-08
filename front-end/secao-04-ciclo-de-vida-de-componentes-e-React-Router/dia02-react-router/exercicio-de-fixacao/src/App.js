71075e25d9f5d2ad48566c24ad7ef3cad5342557
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-04-08 14:14:39 -0300

    Requisito 03 - exercicio de fixaçao

--- front-end/secao-04-ciclo-de-vida-de-componentes-e-React-Router/dia02-react-router/exercicio-de-fixacao/src/App.js
+++ front-end/secao-04-ciclo-de-vida-de-componentes-e-React-Router/dia02-react-router/exercicio-de-fixacao/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={ Home } />
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/about" component={ About } />
       </BrowserRouter>
     );
   }
