268fbd9d8f8099027c6c562b9a474d172528a6ad
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-04-08 17:47:21 -0300

    Requisitos 10 e 11 exercicio de fixação

--- front-end/secao-04-ciclo-de-vida-de-componentes-e-React-Router/dia02-react-router/exercicio-de-fixacao/src/App.js
+++ front-end/secao-04-ciclo-de-vida-de-componentes-e-React-Router/dia02-react-router/exercicio-de-fixacao/src/App.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <nav>
-        <ul>
-          <li><Link to="/" > Home </Link></li>
-          <li><Link to="/about" > About </Link></li>
-          <li><Link to="/users" > Users </Link></li>
-        </ul>
-      </nav>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" />} />
+        <nav>
+          <ul>
+            <li><Link to="/" > Home </Link></li>
+            <li><Link to="/about" > About </Link></li>
+            <li><Link to="/users" > Users </Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" />} />
+          <Route path="/about" component={ About } />
+          <Route exact path="/" component={ Home } />
+        </Switch>
       </BrowserRouter>
     );
   }
