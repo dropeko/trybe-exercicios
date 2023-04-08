c517db952d329ad78016844707e9aff807b230cc
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-04-08 16:20:47 -0300

    Requisito 08 e 09 - exercicio de fixaçao

--- front-end/secao-04-ciclo-de-vida-de-componentes-e-React-Router/dia02-react-router/exercicio-de-fixacao/src/App.js
+++ front-end/secao-04-ciclo-de-vida-de-componentes-e-React-Router/dia02-react-router/exercicio-de-fixacao/src/App.js
@@ -3,17 +3,22 @@ import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Link to="/" > Home </Link>
-        <Link to="/about" > About </Link>
-        <Link to="/users" > Users </Link>
+      <nav>
+        <ul>
+          <li><Link to="/" > Home </Link></li>
+          <li><Link to="/about" > About </Link></li>
+          <li><Link to="/users" > Users </Link></li>
+        </ul>
+      </nav>
         <Route exact path="/" component={ Home } />
         <Route path="/about" component={ About } />
-        <Route path="/users" component={ Users } />
+        <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage="Good Morning" />} />
       </BrowserRouter>
     );
   }
--- front-end/secao-04-ciclo-de-vida-de-componentes-e-React-Router/dia02-react-router/exercicio-de-fixacao/src/components/Users.js
+++ front-end/secao-04-ciclo-de-vida-de-componentes-e-React-Router/dia02-react-router/exercicio-de-fixacao/src/components/Users.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
-    const { greetingsMessage } = this.props
+    const { greetingsMessage,
+      match: { params: { id } }
+    } = this.props
     return (
       <div>
         <h2>Users</h2>
-        <p> { greetingsMessage }, My awesome Users component </p>
+        <p> { greetingsMessage }, User {id}, My awesome Users component </p>
       </div>
     );
   }
