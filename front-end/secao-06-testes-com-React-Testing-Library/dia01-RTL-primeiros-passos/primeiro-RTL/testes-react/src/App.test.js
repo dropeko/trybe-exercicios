edc6e7b86a1895359a7d4935035e0ad9de1b84e7
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-04-22 20:50:49 -0300

    Exercicios de fixação dia 01

--- front-end/secao-06-testes-com-React-Testing-Library/dia01-RTL-primeiros-passos/primeiro-RTL/testes-react/src/App.test.js
+++ front-end/secao-06-testes-com-React-Testing-Library/dia01-RTL-primeiros-passos/primeiro-RTL/testes-react/src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('Verificando se existe o campo Email.', () => {
@@ -23,8 +24,27 @@ test('Verificando se existe um botão de enviar', () => {
   render(<App />);
 
   const btnSend = screen.getByTestId('id-send');
-  
+
   expect(btnSend).toBeInTheDocument();
   expect(btnSend).toHaveProperty('type', 'button');
   expect(btnSend).toHaveValue('Enviar');
+});
+
+// Adicione esse teste.
+test('Verificando se o botão e o campo email estão funcionando.', () => {
+  render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = screen.getByTestId('id-send');
+  const inputEmail = screen.getByLabelText('Email');
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
 });
