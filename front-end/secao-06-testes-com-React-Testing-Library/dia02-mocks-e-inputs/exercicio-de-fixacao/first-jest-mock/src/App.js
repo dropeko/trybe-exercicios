2de0bdf2e3f6c013c1bf634256034bcb74b5d73a
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-04-23 20:56:55 -0300

    Arquivos exercicio de fixação dia 02 mocks RTL

--- front-end/secao-06-testes-com-React-Testing-Library/dia02-mocks-e-inputs/exercicio-de-fixacao/first-jest-mock/src/App.js
+++ front-end/secao-06-testes-com-React-Testing-Library/dia02-mocks-e-inputs/exercicio-de-fixacao/first-jest-mock/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      joke: '',
+    };
+    
+    this.fetchJoke = this.fetchJoke.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchJoke();
+   }
+   
+   async fetchJoke() {
+    const API_URL = 'https://icanhazdadjoke.com/';
+    const REQUEST_CONFIG = { headers: { Accept: 'application/json' } };
+
+    const request = await fetch(API_URL, REQUEST_CONFIG)
+    const response = await request.json()
+
+    console.log(response);
+
+    this.setState({ joke: response.joke })
+  }
+
+  render() {
+    const { joke } = this.state;
+
+    return (
+      <div className="App">
+        <p>{joke}</p>
+        <button type="button" onClick={ this.fetchJoke }>New joke</button>
+      </div>
+    );
+  }
 }
 
 export default App;
