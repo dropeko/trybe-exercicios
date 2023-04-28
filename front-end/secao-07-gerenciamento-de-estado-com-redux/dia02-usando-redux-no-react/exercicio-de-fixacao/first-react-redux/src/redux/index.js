52af0167098660b9585550c316cd1e3a3750e905
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-04-28 10:10:48 -0300

    Primeiro React Redux

---
+++ front-end/secao-07-gerenciamento-de-estado-com-redux/dia02-usando-redux-no-react/exercicio-de-fixacao/first-react-redux/src/redux/index.js
@@ -0,0 +1,14 @@
+// ./src/redux/index.js
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const INITIAL_STATE = { count: 0 };
+
+const reducer = (state = INITIAL_STATE, action) => {
+  return state
+};
+
+const store = createStore(reducer, composeWithDevTools());
+
+export default store;
+
