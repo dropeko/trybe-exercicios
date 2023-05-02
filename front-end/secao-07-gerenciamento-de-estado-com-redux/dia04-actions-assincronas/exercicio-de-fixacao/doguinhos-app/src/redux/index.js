6b76ed8177603ea1b4c1dbfd707dba92dbc9bc8b
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-05-02 12:43:30 -0300

    Arquivos Primeiro Redux Thunk

---
+++ front-end/secao-07-gerenciamento-de-estado-com-redux/dia04-actions-assincronas/exercicio-de-fixacao/doguinhos-app/src/redux/index.js
@@ -0,0 +1,8 @@
+// ./src/redux/index.js
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import dogReducer from './reducers/dogReducer'
+
+const store = createStore(dogReducer, composeWithDevTools());
+
+export default store;
