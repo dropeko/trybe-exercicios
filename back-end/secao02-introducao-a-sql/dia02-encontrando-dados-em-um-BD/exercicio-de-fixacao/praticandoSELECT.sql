c8fe1145ddfad1feb248f4334ade736a01b12747
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-05-29 18:57:41 -0300

    Exercicio de fixação SELECT - dia 02 - SQL

---
+++ back-end/secao02-introducao-a-sql/dia02-encontrando-dados-em-um-BD/exercicio-de-fixacao/praticandoSELECT.sql
@@ -0,0 +1,23 @@
+SELECT 
+    *
+FROM
+    sakila.city;
+
+SELECT 
+    first_name, last_name
+FROM
+    sakila.customer;
+
+SELECT 
+    *
+FROM
+    sakila.rental;
+
+SELECT 
+    title AS 'Título',
+    description AS 'Descrição',
+    release_year AS 'Data de lançamento'
+FROM
+    sakila.film;
+
+SHOW TABLES;
