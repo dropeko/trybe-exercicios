d304af815cd40a3f31d59072790c1835800c2a7e
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-05-29 19:04:11 -0300

    Exercicio de fixação CONCAT - dia 02 - SQL

---
+++ back-end/secao02-introducao-a-sql/dia02-encontrando-dados-em-um-BD/exercicio-de-fixacao/praticandoCONCAT.sql
@@ -0,0 +1,8 @@
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS 'Nome Completo'
+FROM
+    sakila.actor;
+
+SELECT CONCAT(title, ' ', release_year) AS 'Lançamento do Filme' FROM sakila.film;
+
+SELECT CONCAT(address, ' ', district) AS 'Endereço' FROM sakila.address;
