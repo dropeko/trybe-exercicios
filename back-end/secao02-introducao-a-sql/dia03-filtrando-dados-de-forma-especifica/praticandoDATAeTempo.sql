d032c8e2253dff3ff624521bd05d995a388f1f83
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-06-01 16:13:01 -0300

    Praticando DATA E TEMPO dia 03 - seção 02 backEnd

---
+++ back-end/secao02-introducao-a-sql/dia03-filtrando-dados-de-forma-especifica/praticandoDATAeTempo.sql
@@ -0,0 +1,25 @@
+# Maneiras de encontrar dados por data
+# Usando a função DATE(coluna_do_tipo_date):
+SELECT * FROM sakila.payment
+WHERE DATE(payment_date) = '2005-07-31';
+
+# Usando LIKE para valores aproximados:
+SELECT * FROM sakila.payment
+WHERE payment_date LIKE '2005-08-20 00:30:52';
+SELECT * FROM sakila.payment
+WHERE payment_date LIKE '2005-07-31%';
+
+# Usando BETWEEN:
+SELECT *
+FROM sakila.payment
+WHERE payment_date BETWEEN '2005-05-26 00:00:00' AND '2005-05-27 23:59:59';
+
+# Selecionando apenas partes de uma data
+-- Teste cada um dos comandos a seguir:
+SELECT DATE(payment_date) FROM sakila.payment; -- YYYY-MM-DD
+SELECT YEAR(payment_date) FROM sakila.payment; -- Ano
+SELECT MONTH(payment_date) FROM sakila.payment; -- Mês
+SELECT DAY(payment_date) FROM sakila.payment; -- Dia
+SELECT HOUR(payment_date) FROM sakila.payment; -- Hora
+SELECT MINUTE(payment_date) FROM sakila.payment; -- Minuto
+SELECT SECOND(payment_date) FROM sakila.payment; -- Segundo
