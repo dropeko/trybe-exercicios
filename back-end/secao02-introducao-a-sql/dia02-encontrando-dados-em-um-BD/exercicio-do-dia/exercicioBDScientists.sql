e9b1556739d6947799c16297947f43d154e56b5f
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-06-01 12:01:00 -0300

    Exercicio 02 - Dia 02 / Seção 02 - BackEnd

--- back-end/secao02-introducao-a-sql/dia02-encontrando-dados-em-um-BD/exercicio-do-dia/exercicioBDScientists.sql
+++ back-end/secao02-introducao-a-sql/dia02-encontrando-dados-em-um-BD/exercicio-do-dia/exercicioBDScientists.sql
@@ -1,4 +1,7 @@
 USE Scientists;
 
 # 1 - Escreva uma query para exibir a string “This is SQL Exercise, Practice and Solution”.
-SELECT 'This is SQL Exercise, Practice and Solution' AS 'Paraphrase'
+SELECT 'This is SQL Exercise, Practice and Solution' AS 'Paraphrase';
+
+# 2 - Escreva uma query para exibir três números em três colunas.
+SELECT Name AS 'Nome Cientista', SSN AS 'Scientist Number' FROM Scientists LIMIT 3;
