8dc562704d86442255ba3500d31d718a61e648b1
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-06-01 12:01:54 -0300

    Exercicio 03 - Dia 02 / Seção 02 - BackEnd

--- back-end/secao02-introducao-a-sql/dia02-encontrando-dados-em-um-BD/exercicio-do-dia/exercicioBDScientists.sql
+++ back-end/secao02-introducao-a-sql/dia02-encontrando-dados-em-um-BD/exercicio-do-dia/exercicioBDScientists.sql
@@ -5,3 +5,6 @@ SELECT 'This is SQL Exercise, Practice and Solution' AS 'Paraphrase';
 
 # 2 - Escreva uma query para exibir três números em três colunas.
 SELECT Name AS 'Nome Cientista', SSN AS 'Scientist Number' FROM Scientists LIMIT 3;
+
+# 3 - Escreva uma query para exibir a soma dos números 10 e 15.
+SELECT 10 + 15 AS 'Soma';
