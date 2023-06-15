4f32787c6146566fc1ff7cd230a440a2a182673e
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-06-15 15:08:51 -0300

    Exercicios de Fixação - INNER JOIN RIGHT JOIN LEFT JOIN

---
+++ back-end/secao03-funcoes-sql-joins-e-normalizacao/dia02-descomplicando-joins/LEFTJOIN-RIGHTJOIN-INNERJOIN.sql
@@ -0,0 +1,41 @@
+# Diferenças de uso INNER JOIN, LEFT JOIN, RIGHT JOIN
+
+# INNER JOIN
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+INNER JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
+
+# RIGHT JOIN
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+RIGHT JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
+
+# LEFT JOIN
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+LEFT JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
+
