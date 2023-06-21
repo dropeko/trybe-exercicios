5efbc1f093a2165a8e93f17f9270f1aa19b13070
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-06-21 14:16:25 -0300

    Hello World tests com Mocha

--- back-end/secao04-introducao-ao-desenvolvimento-web-com-nodeJs/dia03-nodeJs-testes-de-integracao/solar-system-backend-com-Mocha/solar-system-backend/tests/fsUtils.test.js
+++ back-end/secao04-introducao-ao-desenvolvimento-web-com-nodeJs/dia03-nodeJs-testes-de-integracao/solar-system-backend-com-Mocha/solar-system-backend/tests/fsUtils.test.js
@@ -1,2 +1,13 @@
-// MOCHA escreve testes em NODEJS
+// MOCHA escreve testes em NODEJS. Não precisa importar Mocha direto no arquivo de teste.
 // CHAI ferramenta para ajudar a fazer testes mais simples de ler
+const { expect } = require('chai');
+
+const { readMissionData } = require('../src/utils/fsUtils');
+
+describe('A função readMissionData', function() {
+  it('testa se retorna um array', async function() {
+    const missionData = await readMissionData();
+
+    expect(missionData).instanceOf(Array);
+  });
+});
