6ff78247470c8e38a07b489aac6ec9a04b2026d2
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-06-21 16:37:36 -0300

    Aplicação completa

---
+++ back-end/secao04-introducao-ao-desenvolvimento-web-com-nodeJs/dia03-nodeJs-testes-de-integracao/integration-tests-cacau-trybe/src/cacauTrybe.js
@@ -0,0 +1,39 @@
+// src/cacauTrybe.js
+const fs = require('fs').promises;
+const { join } = require('path');
+
+// Uma função somente para acessar e ler o arquivo JSON
+const readCacauTrybeFile = async () => {
+  const path = '/files/cacauTrybeFile.json';
+  try {
+    const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
+    return JSON.parse(contentFile);
+  } catch (error) {
+    return null;
+  }
+};
+
+// Função para recuperar as informações lidas na função acima
+const getAllChocolates = async () => {
+  const cacauTrybe = await readCacauTrybeFile();
+  return cacauTrybe.chocolates;
+};
+
+const getChocolateById = async (id) => {
+  const cacauTrybe = await readCacauTrybeFile();
+  return cacauTrybe.chocolates
+    .find((chocolate) => chocolate.id === id);
+};
+
+const getChocolatesByBrand = async (brandId) => {
+  const cacauTrybe = await readCacauTrybeFile();
+  return cacauTrybe.chocolates
+    .filter((chocolate) => chocolate.brandId === brandId);
+};
+
+
+module.exports = {
+    getAllChocolates,
+    getChocolateById,
+    getChocolatesByBrand,
+};
