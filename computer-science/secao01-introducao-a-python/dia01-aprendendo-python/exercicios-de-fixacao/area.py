d1d45f967709a4af43c4ab367bfd1669e971331d
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-09-22 19:55:22 -0300

    Cria, importa e usa o primeiro módulo em py

---
+++ computer-science/secao01-introducao-a-python/dia01-aprendendo-python/exercicios-de-fixacao/area.py
@@ -0,0 +1,26 @@
+# Aviso: Existe uma convenção de declarar valores considerados constantes
+# com letras maiúsculas, e o respeito por outros programadores de não
+# alterarem aquele valor.
+
+PI = 3.14  # PI é uma "constante" em nosso módulo
+
+
+def square(side):
+    '''Calculate area of square.'''
+    return side * side
+
+
+def rectangle(length, width):
+    '''Calculate area of rectangle.'''
+    return length * width
+
+
+def circle(radius):
+    '''Calculate area of circle.'''
+    return PI * radius * radius
+
+
+if __name__ == "__main__":
+    print("Área do quadrado:", square(10))
+    print("Área do retângulo:", rectangle(2, 2))
+    print("Área do círculo:", circle(3))
