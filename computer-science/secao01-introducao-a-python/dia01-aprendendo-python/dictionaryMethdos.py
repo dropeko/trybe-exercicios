8ce873dbb3fdaed75618b8dd0d262620e7067e69
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-09-22 11:46:38 -0300

    Metodos de tuplas, conjuntos, frozensets, dicionarios e range em python

---
+++ computer-science/secao01-introducao-a-python/dia01-aprendendo-python/dictionaryMethdos.py
@@ -0,0 +1,33 @@
+# Estrutura que associa uma chave a um determinado valor. É a representação do tão famoso objeto que utilizamos em JavaScript.
+# De olho na dica👀: Em Python, precisamos colocar a chave do objeto entre aspas.
+
+people_by_id = {1: "Maria", 2: "Fernanda", 3: "Felipe"}  # elementos no formato "chave: valor" separados por vírgula, envolvidos por chaves
+
+people_by_name = {"Maria": 1, "Fernanda": 2, "Felipe": 3}  # outro exemplo, dessa vez usando strings como chaves. As aspas são necessárias para que o Python não ache que `Maria`, `Fernanda` e `Felipe` sejam variáveis.
+
+# elementos são acessados por suas chaves
+people_by_id[1]  # saída: Maria
+
+# elementos podem ser removidos com a palavra chave del
+del people_by_id[1]
+people_by_id.items()  # dict_items([(2, "Fernanda"), (3, "Felipe")])
+# é retornada uma coleção iterável de tuplas contendo chaves e valores
+
+# A estrutura mais recomendada para armazenar a contagem da frequência que um elemento aparece em uma sequência é o Dict. Veja abaixo uma possível solução para essa questão:
+my_array = [20, 20, 1, 2]
+
+freq_dict = {}
+
+for item in my_array:
+    if (item in freq_dict):
+        freq_dict[item] += 1
+    else:
+        freq_dict[item] = 1
+
+for key, valor in freq_dict.items():
+    print(f"{key} : {valor}")
+
+# Saída
+20: 2
+1: 1
+2: 1
--- computer-science/secao01-introducao-a-python/dia01-aprendendo-python/listMethods.py
+++ computer-science/secao01-introducao-a-python/dia01-aprendendo-python/listMethods.py
@@ -1,3 +1,5 @@
+# uma lista é uma sequência mutável e ordenada de elementos. Ela pode armazenar elementos heterogêneos, ter seu tamanho variável e crescer à medida que itens são adicionados.
+
 fruits = ["laranja", "maçã", "uva", "abacaxi"]  # elementos são definidos separados por vírgula, envolvidos por colchetes
 
 fruits[0]  # o acesso é feito por índices iniciados em 0
