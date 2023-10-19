a38d34a657475c73fee96b3ccd3733f64c3b61d5
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-10-19 16:18:00 -0300

    Poligon Calculator

---
+++ eletivas-C#/bloco01-fundamentos-e-recursos-C#/dia02-variaveis-com-tipo-condicionais-e-loops/PoligonCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+  public static void Main()
+  {
+      Console.WriteLine("LEITOR DE POLÍGONOS");
+      Console.WriteLine("--------------------");
+
+      Console.WriteLine("Insira o número de lados:");
+      string? numOfSizes = Console.ReadLine();
+
+      int poligonSizes = 0;
+
+      var convertedSizes = Int32.TryParse(numOfSizes, out poligonSizes);
+
+      switch (poligonSizes)
+      {
+        case < 3:
+            Console.WriteLine("Não é um polígono");
+            break;
+        case 3:
+            Console.WriteLine("Triângulo");
+            break;
+        case 4:
+            Console.WriteLine("Quadrado");
+            break;
+        case 5:
+            Console.WriteLine("Pentágono");
+            break;
+        default:
+            Console.WriteLine("WTF is that");
+            break;
+      }
+    }
+}
