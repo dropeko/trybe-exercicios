cfecdaa4d61bf7ca9321441a2c519b524f162b30
Pedro Andrade <pedrocoli_andrade@hotmail.com>
2023-10-23 10:41:47 -0300

    Cria lógica MyExpenses

--- eletivas-C#/bloco01-fundamentos-e-recursos-C#/dia03-arrays-funcoes-e-excecoes/MyExpenses/Program.cs
+++ eletivas-C#/bloco01-fundamentos-e-recursos-C#/dia03-arrays-funcoes-e-excecoes/MyExpenses/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+  public int[] expensesCost;
+  static void Main(string[] args)
+  {
+    int[] expensesCost;
+    Console.WriteLine("Entre com o número de despesas: ");
+    int numberOfExpenses = getNumberOfExpenses();
+
+    for (int i=0; i < numberOfExpenses; i++)
+    {
+      expensesCost[i] = getExpenseCostFromUser();
+    }
+
+    int totalExpenses = 0;
+
+    for (int i = 0; i < numberOfExpenses; i++)
+    {
+      totalExpenses += expensesCost[i];
+    }
+
+    Console.WriteLine("O total das despesas é: " + totalExpenses);
+  }
+}
